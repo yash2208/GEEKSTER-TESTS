ae6a2ae29d200f10977ddcfd3219ca7a4a118ff7
YASH PATIDAR <yashpatidar2208@gmail.com>
2022-05-29 21:09:18 +0530

    test

---
+++ test_29th_may/Question_4/q4.js
@@ -0,0 +1,6 @@
+let num=84; 
+if(num%2===0){   //logic to check, if remainder when num is divided by 2 is zero= even else odd
+    console.log('The number'+' '+num+' '+'is an even number.');
+}else{
+    console.log('The number'+' '+num+ ' '+'is an odd number.');
+}
