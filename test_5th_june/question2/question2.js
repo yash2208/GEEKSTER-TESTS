578d3cfc2d35a3c86b0f306ea9d213957f65531b
YASH PATIDAR <yashpatidar2208@gmail.com>
2022-06-05 21:18:02 +0530

    test

---
+++ test_5th_june/question2/question2.js
@@ -0,0 +1,11 @@
+function sum_Of_Cubes(n) {
+    var sumn = 0;
+  
+    for (var i = 1; i <= n; i++) {
+      sumn += Math.pow(i, 3);
+    }
+    return sumn;
+  }
+  
+  console.log(sum_Of_Cubes(3));
+  console.log(sum_Of_Cubes(4));
---
+++ test_5th_june/question3/question3.js
@@ -0,0 +1,6 @@
+var i, sum = 0;
+for (i = 1; i <= 20; i += 2){
+    console.log(i);
+  sum += i;
+}
+console.log('sum is'+' '+ sum);
